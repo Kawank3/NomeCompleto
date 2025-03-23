5b36a51c54d2f62db2fd0c832cfebc222205e16b
Kawan Weber <kawan_weber@hotmail.com>
2025-03-22 23:29:40 -0300

    Meu Nome

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome;
+string sobrenome;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome Completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
